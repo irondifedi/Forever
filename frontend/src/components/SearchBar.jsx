ab1222c4bdd697e2ebd49f6284f1243906257155
Victor <irondivictor7@gmail.com>
2025-10-21 16:16:29 +0100

    Second changes on ShopContext spelling

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,35 +1,41 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/shopContext'
-import { assets } from '../assets/assets';
-import { useLocation } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
+  const { search, setSearch, showSearch, setShowSearch } =
+    useContext(ShopContext);
+  const [visible, setVisible] = useState(false);
+  const location = useLocation();
 
-    const {search, setSearch, showSearch,setShowSearch} = useContext(ShopContext);
-    const [visible,setVisible] = useState(false)
-   const location = useLocation();
-
-    useEffect(()=>{
-        if (location.pathname.includes('collection')) {
-            setVisible(true)
-        }
-        else {
-            setVisible(false)
-        }
-        
-
-    },[location])
+  useEffect(() => {
+    if (location.pathname.includes("collection")) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [location]);
   return showSearch && visible ? (
-    <div className='border-t border-b bg-gray-50 text-center'>
-        <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 '>
-            <input className='flex-1 outline-none bg-inherit text-sm' value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder='Search' />
-            <img src={assets.search_icon} className='w-4' alt="" />
-        </div>
-        <img onClick={()=>setShowSearch(false)} src={assets.cross_icon} className='w-3 cursor-pointer inline' alt="" />
-
-
+    <div className="border-t border-b bg-gray-50 text-center">
+      <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 ">
+        <input
+          className="flex-1 outline-none bg-inherit text-sm"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Search"
+        />
+        <img src={assets.search_icon} className="w-4" alt="" />
+      </div>
+      <img
+        onClick={() => setShowSearch(false)}
+        src={assets.cross_icon}
+        className="w-3 cursor-pointer inline"
+        alt=""
+      />
     </div>
-  ) : null
-}
+  ) : null;
+};
 
-export default SearchBar
+export default SearchBar;
